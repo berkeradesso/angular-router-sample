0231bd85627b822af65225eecb5e8cbef15b0536
Berker Topuz <berker.topuz@adesso.com.tr>
2022-03-02 13:26:54 +0300

    başlangıç versiyonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { TrainerListComponent } from './trainer-list/trainer-list.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { TrainerDetailsComponent } from './trainer-details/trainer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    TrainerListComponent,
+    PokemonDetailsComponent,
+    TrainerDetailsComponent
   ],
   imports: [
     BrowserModule,
